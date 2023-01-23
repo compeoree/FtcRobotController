d7faa796af2144cd7ddf79990482960c09da963c
Louis <compeoree@users.noreply.github.com>
2023-01-22 18:29:53 -0600

    Trig mecanum teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOP.java
@@ -1,9 +1,22 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+import android.os.SystemClock;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorMRRangeSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
 @TeleOp(name="teleOP!!!!!!!!")
@@ -16,16 +29,25 @@ public class teleOP extends OpMode {
     public static DcMotor bRightDT = null;
     //public static DcMotor Lift     = null;
     //public static DcMotor Claw     = null;
+    public static Servo scan = null;
+    public static DigitalChannel red = null;
+    public static DigitalChannel green = null;
+    public static RevTouchSensor touch = null;
+    public static Rev2mDistanceSensor range = null;
+    public static ColorSensor color = null;
+    public static IMU imu = null;
 
 
-    public double speedMode = 1;
+    public double speedMode = 0.9;
     public boolean xIsHeld = false;
     public boolean bIsHeld = false;
     public boolean dpadLeftIsHeld = false;
     public boolean dpadRightIsHeld = false;
     public double spd = 0.4;
 
-
+    public long lastTime = 0;
+    public boolean lockerA = false;
+    public boolean lockerB = false;
 
 
 
@@ -42,7 +64,23 @@ public class teleOP extends OpMode {
         bRightDT  = hardwareMap.get(DcMotor.class, "BackR");
         //Lift      = hardwareMap.get(DcMotor.class, "lift"    );
         //Claw      = hardwareMap.get(DcMotor.class,   "claw"    );
+        scan = hardwareMap.get(Servo.class, "scanner");
+        red = hardwareMap.get(DigitalChannel.class, "red");
+        green = hardwareMap.get(DigitalChannel.class, "green");
+        touch = hardwareMap.get(RevTouchSensor.class, "touch");
+        range = hardwareMap.get(Rev2mDistanceSensor.class, "range");
+        color = hardwareMap.get(ColorSensor.class, "color");
+        imu = hardwareMap.get(IMU.class, "imu");
 
+        scan.setPosition(0.5);
+
+        color.enableLed(false);
+
+        imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        red.setMode(DigitalChannel.Mode.OUTPUT);
+        green.setMode(DigitalChannel.Mode.OUTPUT);
+        red.setState(true);
+        green.setState(true);
 
 
         tRightDT.setDirection(DcMotor.Direction.REVERSE);
@@ -103,21 +141,49 @@ public class teleOP extends OpMode {
     public void loop() {
 
         //Slow Mode Code for a and b keys
-        if (gamepad1.a) {
-            speedMode = .6;
-        }
-        if (gamepad1.b) {
-            speedMode = 1;
-        }
+//        if (gamepad1.a) {
+//            speedMode = .6;
+//        }
+//        if (gamepad1.b) {
+//            speedMode = 1;
+//        }
         //Slow Mode Code for a and b keys
 
         //Slow Mode Code for bumpers
-        if (gamepad1.right_bumper && speedMode > .2) {
-            speedMode -= .05;
-        } else if (gamepad1.right_trigger >= .5 && speedMode < 2) {
-            speedMode += .05;
-        }
+//        if (gamepad1.right_bumper && speedMode > .2) {
+//            speedMode -= .05;
+//        } else if (gamepad1.right_trigger >= .5 && speedMode < 2) {
+//            speedMode += .05;
+//        }
         //Slow Mode Code for bumpers
+        if (gamepad1.a) {
+            if (!lockerA) {
+                lockerA = true;
+                green.setState(!green.getState());
+            }
+        }
+        else
+            lockerA = false;
+
+        if (gamepad1.b) {
+            if (!lockerB) {
+                lockerB = true;
+                red.setState(!red.getState());
+            }
+        }
+        else
+            lockerB = false;
+
+        if (gamepad1.x && !gamepad1.y) {
+            scan.setPosition(0.4);
+        }
+
+        if (gamepad1.y && !gamepad1.x) {
+            scan.setPosition(0.6);
+        }
+
+        if (!gamepad1.y && ! gamepad1.x)
+            scan.setPosition(0.5);
 
         double stopBuffer = 0; //Not currently Implemented
 
@@ -210,6 +276,12 @@ public class teleOP extends OpMode {
         telemetry.addLine("Back left encoder counts: " + bLeftDT.getCurrentPosition());
         telemetry.addLine("Front right encoder counts: " + tRightDT.getCurrentPosition());
         telemetry.addLine("Back right encoder counts: " + bRightDT.getCurrentPosition());
+        telemetry.addLine("Distance: " + range.getDistance(DistanceUnit.CM));
+        telemetry.addLine("Angle: " + imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+
+        long currentTime = SystemClock.currentThreadTimeMillis();
+        telemetry.addLine("Loop Time (ms):" + (currentTime - lastTime));
+        lastTime = currentTime;
 //        telemetry.addLine("Lift encoder counts: " + Lift.getCurrentPosition());
 //        telemetry.addLine("Claw encoder counts: " + Claw.getCurrentPosition());
 
@@ -230,5 +302,10 @@ public class teleOP extends OpMode {
         bLeftDT.setPower(-cos);
         tRightDT.setPower(-cos);
         bRightDT.setPower(-sin);
+    }
+
+    void scan() {
+        //SystemClock.currentThreadTimeMillis()
+        scan.setPosition(0);
     }
 }
